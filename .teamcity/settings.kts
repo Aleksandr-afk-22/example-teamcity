1e7212144e562e56005818d8d3a177a63498cb1a
Aleksandr <asimaev@gmail.com>
2026-09-02 09:05:19 +0300

    Fix DSL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,6 @@
-import jetbrains.buildServer.configs.kotlin.v2024_07.*
-import jetbrains.buildServer.configs.kotlin.v2024_07.vcs.*
-import jetbrains.buildServer.configs.kotlin.v2024_07.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.*
 
 version = "2024.07"
 
@@ -17,6 +17,7 @@ project {
         }
 
         steps {
+            // Только на master → deploy
             maven {
                 name = "Maven Deploy"
                 goals = "clean deploy"
@@ -24,12 +25,10 @@ project {
                     equals("teamcity.build.branch", "master")
                 }
             }
+            // На всех ветках → test
             maven {
                 name = "Maven Test"
                 goals = "clean test"
-                conditions {
-                    notEquals("teamcity.build.branch", "master")
-                }
             }
         }
     }
