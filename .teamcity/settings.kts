2d8001a2c1580fbc8f1add7d4dc89d30d1a0d4fc
Aleksandr <asimaev@gmail.com>
2026-09-02 09:07:06 +0300

    Fix DSL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -10,24 +10,20 @@ project {
         name = "Build"
 
         vcs {
-            root(GitVcsRoot {
+            root(GitVcsRoot(id = "GitHubFork") {
                 name = "example-teamcity"
                 url = "https://github.com/Aleksandr-afk-22/example-teamcity.git"
             })
         }
 
         steps {
-            // Только на master → deploy
-            maven {
-                name = "Maven Deploy"
+            maven(name = "Maven Deploy") {
                 goals = "clean deploy"
                 conditions {
                     equals("teamcity.build.branch", "master")
                 }
             }
-            // На всех ветках → test
-            maven {
-                name = "Maven Test"
+            maven(name = "Maven Test") {
                 goals = "clean test"
             }
         }
